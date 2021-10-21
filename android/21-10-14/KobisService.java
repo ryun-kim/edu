a851ca22cd314027c348abbde98354c707d3b8b7
ryun-kim <85739335+ryun-kim@users.noreply.github.com>
2021-10-21 16:50:02 +0900

    Create KobisService.java

---
+++ android/21-10-14/KobisService.java
@@ -0,0 +1,18 @@
+package com.koreait.first.ch10;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface KobisService {
+    // 일별 박스오피스
+    @GET("boxoffice/searchDailyBoxOfficeList.json")
+    Call<BoxOfficeResultBodyVO> boxofficeSearchDailyBoxOfficeList
+    (@Query("key") String key, @Query("targetDt") String targetDt);
+
+    // 주간 박스오피스
+    @GET("boxoffice/searchWeeklyBoxOfficeList.json?weekGb=0")
+    Call<BoxOfficeResultBodyVO> searchWeeklyBoxOfficeList
+    (@Query("key") String key, @Query("targetDt") String targetDt,
+     @Query("weekGb") String weekGb);
+}
